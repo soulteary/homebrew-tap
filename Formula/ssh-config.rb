1a8c4f1811c28b8153e4a54fdad146cb214d5773
Su Yang <soulteary@users.noreply.github.com>
2026-08-27 15:36:13 +0800

    fix: update ssh-config formula to v3.0.0

--- Formula/ssh-config.rb
+++ Formula/ssh-config.rb
@@ -1,31 +1,15 @@
 class SshConfig < Formula
   desc "Manage SSH config with YAML/JSON round-trip support"
   homepage "https://github.com/soulteary/ssh-config"
-  version "2.0.0"
+  url "https://github.com/soulteary/ssh-config/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "2ecc8caf146819c9b16e09a1f430050b98619f284b28acc3230be1d39488a5aa"
   license "Apache-2.0"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/soulteary/ssh-config/releases/download/v2.0.0/ssh-config_Darwin_arm64.tar.gz"
-      sha256 "1b086f8c551761a085be15f35b54c62c6a74571e5fd73c015f0a871a252b4319"
-    else
-      url "https://github.com/soulteary/ssh-config/releases/download/v2.0.0/ssh-config_Darwin_x86_64.tar.gz"
-      sha256 "e5972713c1d88b3a47feee5f6a7a2c71e6f4216e45236dc7909044566b844bb4"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/soulteary/ssh-config/releases/download/v2.0.0/ssh-config_Linux_arm64.tar.gz"
-      sha256 "8f9bfa3204c903e480d96e8c86c0b47259af051a941c13cb79a31af4faaa63b2"
-    else
-      url "https://github.com/soulteary/ssh-config/releases/download/v2.0.0/ssh-config_Linux_x86_64.tar.gz"
-      sha256 "62277ba988a89feab29fc519596e1f875c63dcf1ad12a08a5cd6d5f44aa5514e"
-    end
-  end
+  depends_on "go" => :build
 
   def install
-    bin.install "ssh-config"
+    ldflags = "-s -w -X main.version=#{version} -X main.builtBy=homebrew"
+    system "go", "build", *std_go_args(ldflags:), "."
   end
 
   test do
