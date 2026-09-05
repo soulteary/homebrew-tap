5cd9abea5a815b984bb1a62ef3b945c6ccc078fb
Su Yang <soulteary@users.noreply.github.com>
2026-09-05 18:49:05 +0800

    chore: update portmap

--- Formula/portmap.rb
+++ Formula/portmap.rb
@@ -1,26 +1,26 @@
 class Portmap < Formula
   desc "General-purpose TCP/UDP port forwarding tool written in Go"
   homepage "https://github.com/soulteary/portmap"
-  version "1.1.0"
+  version "1.2.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/soulteary/portmap/releases/download/v1.1.0/portmap_1.1.0_darwin_arm64.tar.gz"
-      sha256 "50345077deb30f696876f96a317c7abe6bf441426d7441315f0c6ae4ff39a522"
+      url "https://github.com/soulteary/portmap/releases/download/v1.2.0/portmap_1.2.0_darwin_arm64.tar.gz"
+      sha256 "5d2ba7df705334ae595c0e56bc9011c14120e75cd52e590275a1680314825ea9"
     else
-      url "https://github.com/soulteary/portmap/releases/download/v1.1.0/portmap_1.1.0_darwin_amd64.tar.gz"
-      sha256 "59797e0c46eb864e3f106bc3fd292e798246fdfe64bf59c9a9eb4b70e0dc6f89"
+      url "https://github.com/soulteary/portmap/releases/download/v1.2.0/portmap_1.2.0_darwin_amd64.tar.gz"
+      sha256 "1df81d086e85ba59a4736dea8134fd9cfaee4af0873c4d9529599faf66a52efc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/soulteary/portmap/releases/download/v1.1.0/portmap_1.1.0_linux_arm64.tar.gz"
-      sha256 "25def0c12087623ba65f4881ea231ab8bb33ca416231083ae61cb76683fad3d6"
+      url "https://github.com/soulteary/portmap/releases/download/v1.2.0/portmap_1.2.0_linux_arm64.tar.gz"
+      sha256 "11b36279901984e118686f055f76a2dd7ca201996c87f21dcf4ef12baa73d949"
     else
-      url "https://github.com/soulteary/portmap/releases/download/v1.1.0/portmap_1.1.0_linux_amd64.tar.gz"
-      sha256 "355eca4c36359a6b189247f5b7a63e04cff28e0097c0e305b830f95c511557c2"
+      url "https://github.com/soulteary/portmap/releases/download/v1.2.0/portmap_1.2.0_linux_amd64.tar.gz"
+      sha256 "791cbb18557f2023d8c6195e38d1f08eaf3f42e5c9b8a5ac0cd8c0cfa444568e"
     end
   end
 
